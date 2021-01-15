c3b77c0114ff1856316b9febc19b6c673f6ff949
IbraheemLanre <s2001108@edu.bc.fi>
2021-01-15 13:16:07 +0200

    first task in ttypescript

---
+++ script.js
@@ -0,0 +1,6 @@
+var firstname = 'Donald';
+var lastname = 'Trump';
+function combine(a, b) {
+    console.log('Full name is: ' + a + ' ' + b);
+}
+combine(firstname, lastname);
---
+++ script.ts
@@ -0,0 +1,8 @@
+var firstname:string = 'Donald'
+var lastname:string = 'Trump'
+
+function combine(a:string, b:string){  
+    console.log('Full name is: ' + a + ' ' + b)
+}
+
+combine(firstname, lastname)
